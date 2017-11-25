46ea155e08effb60e0b120c5f0dbc09312c898c9
Tawny Sit <tsit@caltech.edu>
2017-11-25 02:04:31 -0800

    Take command line input and save individual plots to files

--- euler.py
+++ euler.py
@@ -1,12 +1,6 @@
-"""
-this program performs the explicit or implicit Euler integration for a mass on a spring
-it also can calculate the analytic solution to the problem
-using these solutions, global and truncation error and normalized energy can be calculated
-can choose which values to plot by uncommenting lines at the end
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 # calculates mass on spring using explicit Euler method
 def explicit(start, end, step_size, x_0, v_0):
@@ -81,20 +75,34 @@ def energy(start, end, step_size, x, v):
         energy[i] = x[i]**2 + v[i]**2
     return energy
 
+# calculates mass on spring using sympletic Euler method
+def sympletic(start, end, step_size, x_0, v_0):
+    steps = (end-start)/step_size
+    timesteps = np.linspace(start, end, steps+1)
+    x_values = np.zeros(len(timesteps))
+    v_values = np.zeros(len(timesteps))
+    x_values[0] = x_0
+    v_values[0] = v_0
+    for i in range(len(timesteps)-1):
+        x_values[i+1] = x_values[i] + (step_size * v_values[i])
+        v_values[i+1] = v_values[i] - (step_size * x_values[i+1])
+    return x_values, v_values
+
 #set initial parameters
-start = 0
-end = 2
-step_size = .1
-x_0 = 1
-v_0 = 0
+start = float(sys.argv[1]) #0
+end = float(sys.argv[2]) #20
+step_size = float(sys.argv[3]) #.1
+x_0 = float(sys.argv[4]) #1
+v_0 = float(sys.argv[5]) #0
 
 # creates array of t values, same as in functions defined above
 steps = (end-start)/step_size
 timesteps = np.linspace(start, end, steps+1)
 
-# calculate explicit and implicit values for x and v
+# calculate explicit, implicit, sympletic values for x and v
 x_explicit, v_explicit = explicit(start, end, step_size, x_0, v_0)
 x_implicit, v_implicit = implicit(start, end, step_size, x_0, v_0)
+x_sympletic, v_sympletic = sympletic(start, end, step_size, x_0, v_0)
 
 # calculate analytic solution
 x_analytic, v_analytic = analytic(start, end, step_size, x_0, v_0)
@@ -105,24 +113,129 @@ error_x_implicit, error_v_implicit = global_error(x_analytic, v_analytic, x_impl
 energy_explicit = energy(start, end, step_size, x_explicit, v_explicit)
 energy_implicit = energy(start, end, step_size, x_implicit, v_implicit)
 
+# calculate energies for sympletic and analytic solutions
+energy_analytic = energy(start, end, step_size, x_analytic, v_analytic)
+energy_sympletic = energy(start, end, step_size, x_sympletic, v_sympletic)
+
 #calculate truncation error
 h_values, truncation_error = truncation_error(step_size, end, x_0, v_0)
 
-# uncomment line(s) that you wish to plot:
-
-#plt.plot(timesteps, x_explicit, '-', label='x (explicit)') # plot numerical x values from explicit integration
-#plt.plot(timesteps, v_explicit, '-', label='v (explicit)') # plot numerical v values from explicit integration
-#plt.plot(timesteps, x_implicit, '-', label='x (implicit)') # plot numerical x values from implicit integration
-#plt.plot(timesteps, v_implicit, '-', label='v (implicit)') # plot numerical v values from implicit integration
-#plt.plot(timesteps, x_analytic, '-', label='x analytic') # plot analytic x values
-#plt.plot(timesteps, v_analytic, '-', label='v analytic') # plot analytic v values
-#plt.plot(timesteps, error_x_explicit, '-', label='explicit x error') # plot global error in x from explicit integration
-#plt.plot(timesteps, error_v_explicit, '-', label='explicit v error') # plot global error in v from explicit integration
-#plt.plot(timesteps, error_x_implicit, '-', label='implicit x error') # plot global error in x from implicit integration
-#plt.plot(timesteps, error_v_implicit, '-', label='implicit v error') # plot global error in v from implicit integration
-#plt.plot(h_values, truncation_error, 'o') # plot truncation error as function of step size
-#plt.plot(timesteps, energy_explicit, '-') # plot energy of numerical integration from explicit integration
-#plt.plot(timesteps, energy_implicit, '-') # plot energy of numerical integration from implicit integration
-
+# plot and save x and v values from explicit integration
+plt.plot(timesteps, x_explicit, '-', label='x (explicit)')
+plt.plot(timesteps, v_explicit, '-', label='v (explicit)')
 plt.legend()
-plt.show()
+plt.savefig("explicit_solution.jpg")
+plt.close()
+
+# plot and save x and v values from implicit integration
+plt.plot(timesteps, x_implicit, '-', label='x (implicit)')
+plt.plot(timesteps, v_implicit, '-', label='v (implicit)')
+plt.legend()
+plt.savefig("implicit_solution.jpg")
+plt.close()
+
+# plot and save x and v values from analytic solution
+plt.plot(timesteps, x_analytic, '-', label='x (analytic)')
+plt.plot(timesteps, v_analytic, '-', label='v (analytic)')
+plt.legend()
+plt.savefig("analytic_solution.jpg")
+plt.close()
+
+# plot and save explicit vs implicit solution
+plt.plot(timesteps, x_explicit, '-', label='x (explicit)')
+plt.plot(timesteps, v_explicit, '-', label='v (explicit)')
+plt.plot(timesteps, x_implicit, '-', label='x (implicit)')
+plt.plot(timesteps, v_implicit, '-', label='v (implicit)')
+plt.legend()
+plt.savefig("implicit_vs_explicit.jpg")
+plt.close()
+
+# plot and save analytic vs explicit solution
+plt.plot(timesteps, x_analytic, '-', label='x (analytic)')
+plt.plot(timesteps, v_analytic, '-', label='v (analytic)')
+plt.plot(timesteps, x_explicit, '-', label='x (explicit)')
+plt.plot(timesteps, v_explicit, '-', label='v (explicit)')
+plt.legend()
+plt.savefig("analytic_vs_explicit.jpg")
+plt.close()
+
+# plot and save analytic vs implicit solution
+plt.plot(timesteps, x_analytic, '-', label='x (analytic)')
+plt.plot(timesteps, v_analytic, '-', label='v (analytic)')
+plt.plot(timesteps, x_implicit, '-', label='x (implicit)')
+plt.plot(timesteps, v_implicit, '-', label='v (implicit)')
+plt.legend()
+plt.savefig("analytic_vs_implicit.jpg")
+plt.close()
+
+# plot and save errors from explicit integration
+plt.plot(timesteps, error_x_explicit, '-', label='explicit x error')
+plt.plot(timesteps, error_v_explicit, '-', label='explicit v error')
+plt.legend()
+plt.savefig("explicit_error.jpg")
+plt.close()
+
+# plot and save errors from implicit integration
+plt.plot(timesteps, error_x_implicit, '-', label='implicit x error')
+plt.plot(timesteps, error_v_implicit, '-', label='implicit v error')
+plt.legend()
+plt.savefig("implicit_error.jpg")
+plt.close()
+
+# plot and save errors from implicit and explicit integration
+plt.plot(timesteps, error_x_explicit, '-', label='explicit x error')
+plt.plot(timesteps, error_v_explicit, '-', label='explicit v error')
+plt.plot(timesteps, error_x_implicit, '-', label='implicit x error')
+plt.plot(timesteps, error_v_implicit, '-', label='implicit v error')
+plt.legend()
+plt.savefig("implicit_vs_explicit_error.jpg")
+plt.close()
+
+# plot and save truncation error
+plt.plot(h_values, truncation_error, 'o')
+plt.savefig("truncation_error.jpg")
+plt.close()
+
+# plot energy from explicit integration
+plt.plot(timesteps, energy_explicit, '-')
+plt.savefig("explicit_energy.jpg")
+plt.close()
+
+# plot energy from implicit integration
+plt.plot(timesteps, energy_implicit, '-')
+plt.savefig("implicit_energy.jpg")
+plt.close()
+
+# implicit, explicit, analytic phasespace
+plt.plot(x_explicit, v_explicit, '-', label='explicit')
+plt.plot(x_implicit, v_implicit, '-', label='implicit')
+plt.plot(x_analytic, v_analytic, '-', label='analytic')
+plt.legend()
+plt.savefig("implicit_vs_explicit_vs_analytic_phasespace.jpg")
+plt.close()
+
+# sympletic phasespace
+plt.plot(x_sympletic, v_sympletic, '-', label='sympletic')
+plt.legend()
+plt.savefig("sympletic_phasespace.jpg")
+plt.close()
+
+# analytic phasespace
+plt.plot(x_analytic, v_analytic, '-', label='analytic')
+plt.legend()
+plt.savefig("analytic_phasespace.jpg")
+plt.close()
+
+# analytic vs sympletic phasespace
+plt.plot(x_sympletic, v_sympletic, '-', label='sympletic')
+plt.plot(x_analytic, v_analytic, '-', label='analytic')
+plt.legend()
+plt.savefig("analytic_vs_sympletic_phasespace.jpg")
+plt.close()
+
+# plot energies of sympletic and analytic solutions
+plt.plot(timesteps, energy_analytic, '-', label='analytic')
+plt.plot(timesteps, energy_sympletic, '-', label='sympletic')
+plt.legend()
+plt.savefig("analytic_vs_sympletic_energy.jpg")
+plt.close()
